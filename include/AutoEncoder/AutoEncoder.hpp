b642357043a032f50790cc4a1c78ac68bdbf4a86
cioakeim <cioakeim@ece.auth.gr>
2025-01-02 17:08:14 +0200

    Header struct of AutoEncoder

--- include/AutoEncoder/AutoEncoder.hpp
+++ include/AutoEncoder/AutoEncoder.hpp
@@ -6,11 +6,27 @@
 /**
  * @brief Autoencoder class 
   */
-class AutoEncoder{
+class AutoEncoder:MLP{
 protected:
+  // Stack structure for layers: enc_stack[0]==in / dec_stack[0]==out
+  // Both vectors point to the same objects layers[] sees.
+  std::vector<std::shared_ptr<BaseLayer>> enc_stack;
+  std::vector<std::shared_ptr<BaseLayer>> dec_stack;
+  std::shared_ptr<MatrixXf> encoded; //< Pointer to encoded output
+
 
 public:
-  AutoEncoder(){};
+  AutoEncoder(SampleMatrix& training_set,
+              SampleMatrix& test_set,
+              int batch_size):MLP(training_set,test_set,batch_size){};
+
+  // Add a new layer stack and lock the previous ones
+  void addStackLayer(LayerConfig config);
+
+  // For only encoding matrices
+  MatrixXf encode(MatrixXf& set);
+   
+  
 
 
 };
